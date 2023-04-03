58187e99cc27d33b3628e29d46a572419d73b45e
Carlos Andres Castrillon <caacastr@bancolombia.com.co>
2023-04-03 16:34:59 -0500

    Tarea OK

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const lstIimg = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const li = galleryItems.map(img=>`
+    <li class="gallery__item">
+    <a class="gallery__link" href=${img.original}>
+            <img
+                class="gallery__image"
+                src=${img.preview}
+                data-source=${img.original}
+                alt=${img.description}
+  />
+</a>
+</li>`).join("");
+
+
+function imgClick (event){
+    event.preventDefault();
+    const imgOriginalURL = event.target.dataset.source;
+    basicLightbox.create(`<img src=${imgOriginalURL}>`).show();  
+}
+
+lstIimg.innerHTML=li;
+lstIimg.addEventListener('click', imgClick);
+
+//console.log(galleryItems);
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const lstIimg = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+const li = galleryItems.map(img=>`
+<li class="gallery__item">
+   <a class="gallery__link" href=${img.original}>
+      <img class="gallery__image" src=${img.preview} alt=${img.description} />
+   </a>
+</li>
+`).join("");
+
+lstIimg.innerHTML=li;
+const imgScroll= new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay:250});
+imgScroll.on('show.simplelightbox');
